8716601a2dfb9933632530fbe36b0d84f12c6788
FrancitiX <gamercrepy8@gmail.com>
2024-11-14 02:25:03 -0600

    Update registrar.js

--- tu-proyecto/js/registrar.js
+++ tu-proyecto/js/registrar.js
@@ -1,4 +1,4 @@
-const serverUrl = "http://localhost:5001";
+const serverUrl = "https://francitix.github.io/Node-Test/";
 
 const usuario = localStorage.getItem("usuario");
 const recordar = localStorage.getItem("recordar");
